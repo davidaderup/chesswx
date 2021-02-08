a6e4777be7c839cea29cdef92da50ddc48fef1ab
David Andersson <davand@raysearchlabs.com>
2021-02-08 14:10:42 +0100

    refactored movement calculation to general function

--- bishop.py
+++ bishop.py
@@ -2,19 +2,19 @@ from piece import Piece
 import numpy as np
 
 
-class Rook(Piece):
+class Bishop(Piece):
     def __init__(self, id, owner):
         self.id = id
         self.owner = owner
         self.position = np.array([-1, -1])
-        self.type = "Rook"
+        self.type = "Bishop"
 
     def get_moves(self, board):
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        directions = [[1, 1], [-1, 1], [1, -1], [-1, -1]]
         moves = self.find_moves_from_movement_vectors(board=board,
                                                       directions=directions,
                                                       can_step_multiple=True,
-                                                      incremental_sum=1)
+                                                      incremental_sum=2)
         return moves
 
     def move_to_position(self, position):
--- piece.py
+++ piece.py
@@ -1,6 +1,6 @@
 
 from abc import ABCMeta, abstractmethod
-
+import numpy as np
 
 class Piece():
     def __init__(self, id, owner):
@@ -16,6 +16,30 @@ class Piece():
     @abstractmethod
     def move_to_position(self, position):
         self.position = position
+
+    def find_moves_from_movement_vectors(self, board, directions, can_step_multiple, incremental_sum=1):
+        moves = []
+        direction_ind = -1
+        while direction_ind + 1 < len(directions):
+            direction_ind += 1
+            direction = np.array(directions[direction_ind])
+
+            new_position = self.position + direction
+            new_position = new_position.astype(int)
+            if board.is_out_of_bounds(new_position):
+                continue
+            if board.is_occupied(new_position):
+                occupying_piece = board.get_piece_by_position(new_position)
+                if occupying_piece.owner != self.owner:
+                    moves.append(new_position)
+                continue
+            else:
+                moves.append(new_position)
+
+                if can_step_multiple:
+                    incremental_step = incremental_sum * direction / np.abs(direction).sum()
+                    directions.append(direction + incremental_step)
+        return moves
 
     def __str__(self):
         return f"Type: {self.type}, Owner: {self.owner}, ID: {self.id}, Position: {self.position}"
